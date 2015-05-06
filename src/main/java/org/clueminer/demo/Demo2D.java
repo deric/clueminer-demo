691da250e6d3dca604b2240e0b358e9f3ac4ca02
Tomas Barton <barton.tomas@gmail.com>
2015-05-06 13:24:36 +0200

    simple scatter demo

---
+++ src/main/java/org/clueminer/demo/Demo2D.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2011-2015 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.demo;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import org.clueminer.clustering.api.Clustering;
+import org.clueminer.fixtures.clustering.FakeClustering;
+import org.clueminer.scatter.ScatterPlot2;
+
+/**
+ *
+ * @author deric
+ */
+public class Demo2D extends JPanel {
+
+    private ScatterPlot2 plot;
+
+    public Demo2D() {
+        setPreferredSize(new Dimension(800, 600));
+        initComponents();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new Demo2D().showInFrame();
+            }
+        });
+    }
+
+    public String getTitle() {
+        return "Scatter plot";
+    }
+
+    protected JFrame showInFrame() {
+        JFrame frame = new JFrame(getTitle());
+        frame.getContentPane().add(this, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(500, 500));
+        //frame.setSize(getPreferredSize());
+        frame.setVisible(true);
+        return frame;
+    }
+
+    private void initComponents() {
+        setSize(500, 500);
+        setLayout(new BorderLayout());
+        plot = new ScatterPlot2();
+        Clustering clusters = FakeClustering.iris();
+        plot.setClustering(clusters);
+        add(plot);
+    }
+
+}

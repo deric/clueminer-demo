242b2b9a57a34f90844e319c80c3303aa04f4034
Tomas Barton <barton.tomas@gmail.com>
2015-05-25 09:48:50 +0200

    refactor reading from a jar

--- src/main/java/org/clueminer/demo/data/ResourceList.java
+++ src/main/java/org/clueminer/demo/data/ResourceList.java
@@ -17,16 +17,16 @@
 package org.clueminer.demo.data;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
-import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+import org.openide.util.Exceptions;
 
 /**
  * code from
@@ -77,29 +77,31 @@ public class ResourceList {
 
     private static Collection<String> getResourcesFromJarFile(final File file,
             final Pattern pattern) {
-        final List<String> retval = new ArrayList<>();
-        ZipFile zf;
+        final List<String> retval = new LinkedList<>();
+        ZipInputStream zip = null;
         try {
-            zf = new ZipFile(file);
-        } catch (final ZipException e) {
-            throw new Error(e);
-        } catch (final IOException e) {
-            throw new Error(e);
+            zip = new ZipInputStream(new FileInputStream(file));
+        } catch (FileNotFoundException ex) {
+            Exceptions.printStackTrace(ex);
         }
-        final Enumeration e = zf.entries();
-        while (e.hasMoreElements()) {
-            final ZipEntry ze = (ZipEntry) e.nextElement();
-            final String fileName = ze.getName();
-            final boolean accept = pattern.matcher(fileName).matches();
-            if (accept) {
-                retval.add(fileName);
+
+        if (zip != null) {
+            try {
+                while (zip.available() == 1) {
+                    final ZipEntry ze = zip.getNextEntry();
+                    final String fileName = ze.getName();
+                    final boolean accept = pattern.matcher(fileName).matches();
+                    if (accept) {
+                        retval.add(fileName);
+                    }
+                    zip.closeEntry();
+                }
+                zip.close();
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
             }
         }
-        try {
-            zf.close();
-        } catch (final IOException e1) {
-            throw new Error(e1);
-        }
+
         return retval;
     }
 
